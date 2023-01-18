f671c2f10ebea1a2d794a0df1871eba87b60c15f
s-t-mcdonald <mcdonst@mit.edu>
2023-01-18 11:45:12 -0500

    Update to results df

--- training_pipeline.py
+++ training_pipeline.py
@@ -1,7 +1,9 @@
-import pandas as pd
-import numpy as np
 import sys
 import os
+
+import pandas as pd
+import numpy as np
+from sklearn.metrics import accuracy_score, f1_score, log_loss
 
 from src.const import *
 from src.feature_processing_functions import *
@@ -27,11 +29,32 @@ except:
     None
 y_train, y_val, y_test, train_pred, val_pred, test_pred = train_airport_nn(airport, lookahead, data, config_support, epochs, number_trials, patience, experiment_id, Dir)
 
-np.savetxt(Dir+f"Results/Result_{experiment_id}/y_train_truth.csv", y_train, delimiter=",")
-np.savetxt(Dir+f"Results/Result_{experiment_id}/y_val_truth.csv", y_val, delimiter=",")
-np.savetxt(Dir+f"Results/Result_{experiment_id}/y_test_truth.csv", y_test, delimiter=",")
 
-np.savetxt(Dir+f"Results/Result_{experiment_id}/y_train_pred.csv", train_pred, delimiter=",")
-np.savetxt(Dir+f"Results/Result_{experiment_id}/y_val_pred.csv", val_pred, delimiter=",")
-np.savetxt(Dir+f"Results/Result_{experiment_id}/y_test_pred.csv", test_pred, delimiter=",")
+train_loss = log_loss(y_train.flatten(), train_pred.flatten())
+val_loss = log_loss(y_val.flatten(), val_pred.flatten())
+test_loss = log_loss(y_test.flatten(), test_pred.flatten())
+
+train_accuracy = accuracy_score(np.argmax(y_train,axis=1), np.argmax(train_pred,axis=1))
+val_accuracy = accuracy_score(np.argmax(y_val,axis=1), np.argmax(val_pred,axis=1))
+test_accuracy = accuracy_score(np.argmax(y_test,axis=1), np.argmax(test_pred,axis=1))
+
+train_f1 = f1_score(np.argmax(y_train,axis=1), np.argmax(train_pred,axis=1), average='weighted')
+val_f1 = f1_score(np.argmax(y_val,axis=1), np.argmax(val_pred,axis=1), average='weighted')
+test_f1 = f1_score(np.argmax(y_test,axis=1), np.argmax(test_pred,axis=1), average='weighted')
+
+df_dict = {"train_loss": [train_loss], "val_loss": [val_loss], "test_loss": [test_loss],
+                "train_accuracy": [train_accuracy], "val_accuracy": [val_accuracy], "test_accuracy": [test_accuracy],
+                "train_f1": [train_f1], "val_f1": [val_f1], "test_f1": [test_f1]}
+
+df = pd.DataFrame(df_dict)
+
+df.to_csv(Dir+f"Results/Result_{experiment_id}/results_df.csv")
+
+# np.savetxt(Dir+f"Results/Result_{experiment_id}/y_train_truth.csv", y_train, delimiter=",")
+# np.savetxt(Dir+f"Results/Result_{experiment_id}/y_val_truth.csv", y_val, delimiter=",")
+# np.savetxt(Dir+f"Results/Result_{experiment_id}/y_test_truth.csv", y_test, delimiter=",")
+
+# np.savetxt(Dir+f"Results/Result_{experiment_id}/y_train_pred.csv", train_pred, delimiter=",")
+# np.savetxt(Dir+f"Results/Result_{experiment_id}/y_val_pred.csv", val_pred, delimiter=",")
+# np.savetxt(Dir+f"Results/Result_{experiment_id}/y_test_pred.csv", test_pred, delimiter=",")
 
