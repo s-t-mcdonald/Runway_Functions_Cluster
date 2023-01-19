32c96a1710421f2e74f66b5b31c4678592cc92a9
s-t-mcdonald <mcdonst@mit.edu>
2023-01-18 22:41:14 -0500

    Update to training pipeline

---
+++ run_experiment.py
@@ -0,0 +1,65 @@
+import sys
+import os
+
+import pandas as pd
+import numpy as np
+from sklearn.metrics import accuracy_score, f1_score, log_loss
+
+from src.const import *
+from src.feature_processing_functions import *
+
+from src.nn_pipeline import train_airport_nn
+
+def run_experiment(experiment_id):
+
+    Dir = DIR
+    parameter_array = pd.read_csv(Dir+"parameters/parameter_array.csv").set_index('PARAM')
+
+    airport             = parameter_array.loc[experiment_id]["AIRPORTS"]
+    lookahead           = parameter_array.loc[experiment_id]["LOOKAHEAD"]
+    data                = parameter_array.loc[experiment_id]["DATA"]
+    config_support      = parameter_array.loc[experiment_id]["CONFIG_SUPPORT"]
+    epochs              = parameter_array.loc[experiment_id]["EPOCHS"]
+    number_trials       = parameter_array.loc[experiment_id]["NUMBER_TRIALS"]
+    patience            = parameter_array.loc[experiment_id]["PATIENCE"]
+
+    try:
+        os.mkdir(Dir+f"Results/Result_{experiment_id}")
+    except:
+        None
+    y_train, y_val, y_test, train_pred, val_pred, test_pred = train_airport_nn(airport, lookahead, data, config_support, epochs, number_trials, patience, experiment_id, Dir)
+
+
+    train_loss = log_loss(y_train.flatten(), train_pred.flatten())
+    val_loss = log_loss(y_val.flatten(), val_pred.flatten())
+    test_loss = log_loss(y_test.flatten(), test_pred.flatten())
+
+    train_accuracy = accuracy_score(np.argmax(y_train,axis=1), np.argmax(train_pred,axis=1))
+    val_accuracy = accuracy_score(np.argmax(y_val,axis=1), np.argmax(val_pred,axis=1))
+    test_accuracy = accuracy_score(np.argmax(y_test,axis=1), np.argmax(test_pred,axis=1))
+
+    train_f1 = f1_score(np.argmax(y_train,axis=1), np.argmax(train_pred,axis=1), average='weighted')
+    val_f1 = f1_score(np.argmax(y_val,axis=1), np.argmax(val_pred,axis=1), average='weighted')
+    test_f1 = f1_score(np.argmax(y_test,axis=1), np.argmax(test_pred,axis=1), average='weighted')
+
+    df_dict = {"param": [experiment_id], "train_loss": [train_loss], "val_loss": [val_loss], "test_loss": [test_loss],
+                    "train_accuracy": [train_accuracy], "val_accuracy": [val_accuracy], "test_accuracy": [test_accuracy],
+                    "train_f1": [train_f1], "val_f1": [val_f1], "test_f1": [test_f1]}
+
+    df = pd.DataFrame(df_dict)
+
+    df.to_csv(Dir+f"Results/Result_{experiment_id}/results_df.csv")
+
+if __name__ == "__main__":
+
+    experiment_id = int(sys.argv[1])
+    run_experiment(experiment_id)
+
+# np.savetxt(Dir+f"Results/Result_{experiment_id}/y_train_truth.csv", y_train, delimiter=",")
+# np.savetxt(Dir+f"Results/Result_{experiment_id}/y_val_truth.csv", y_val, delimiter=",")
+# np.savetxt(Dir+f"Results/Result_{experiment_id}/y_test_truth.csv", y_test, delimiter=",")
+
+# np.savetxt(Dir+f"Results/Result_{experiment_id}/y_train_pred.csv", train_pred, delimiter=",")
+# np.savetxt(Dir+f"Results/Result_{experiment_id}/y_val_pred.csv", val_pred, delimiter=",")
+# np.savetxt(Dir+f"Results/Result_{experiment_id}/y_test_pred.csv", test_pred, delimiter=",")
+
--- training_pipeline.py
+++ training_pipeline.py
@@ -1,60 +1,18 @@
-import sys
-import os
-
+import signal
+import multiprocessing
 import pandas as pd
-import numpy as np
-from sklearn.metrics import accuracy_score, f1_score, log_loss
-
-from src.const import *
-from src.feature_processing_functions import *
-
-from src.nn_pipeline import train_airport_nn
-
-experiment_id = int(sys.argv[1])
-
-Dir = DIR
-parameter_array = pd.read_csv(Dir+"parameters/parameter_array.csv").set_index('PARAM')
-
-airport             = parameter_array.loc[experiment_id]["AIRPORTS"]
-lookahead           = parameter_array.loc[experiment_id]["LOOKAHEAD"]
-data                = parameter_array.loc[experiment_id]["DATA"]
-config_support      = parameter_array.loc[experiment_id]["CONFIG_SUPPORT"]
-epochs              = parameter_array.loc[experiment_id]["EPOCHS"]
-number_trials       = parameter_array.loc[experiment_id]["NUMBER_TRIALS"]
-patience            = parameter_array.loc[experiment_id]["PATIENCE"]
-
-try:
-    os.mkdir(Dir+f"Results/Result_{experiment_id}")
-except:
-    None
-y_train, y_val, y_test, train_pred, val_pred, test_pred = train_airport_nn(airport, lookahead, data, config_support, epochs, number_trials, patience, experiment_id, Dir)
+from run_experiment import run_experiment
 
 
-train_loss = log_loss(y_train.flatten(), train_pred.flatten())
-val_loss = log_loss(y_val.flatten(), val_pred.flatten())
-test_loss = log_loss(y_test.flatten(), test_pred.flatten())
+def init_worker():
+    ''' Add KeyboardInterrupt exception to mutliprocessing workers '''
+    signal.signal(signal.SIGINT, signal.SIG_IGN)
 
-train_accuracy = accuracy_score(np.argmax(y_train,axis=1), np.argmax(train_pred,axis=1))
-val_accuracy = accuracy_score(np.argmax(y_val,axis=1), np.argmax(val_pred,axis=1))
-test_accuracy = accuracy_score(np.argmax(y_test,axis=1), np.argmax(test_pred,axis=1))
+param_array = pd.read_csv("parameters/parameter_array.csv")
 
-train_f1 = f1_score(np.argmax(y_train,axis=1), np.argmax(train_pred,axis=1), average='weighted')
-val_f1 = f1_score(np.argmax(y_val,axis=1), np.argmax(val_pred,axis=1), average='weighted')
-test_f1 = f1_score(np.argmax(y_test,axis=1), np.argmax(test_pred,axis=1), average='weighted')
 
-df_dict = {"param": [experiment_id], "train_loss": [train_loss], "val_loss": [val_loss], "test_loss": [test_loss],
-                "train_accuracy": [train_accuracy], "val_accuracy": [val_accuracy], "test_accuracy": [test_accuracy],
-                "train_f1": [train_f1], "val_f1": [val_f1], "test_f1": [test_f1]}
+EXPERIMENT_IDS = [1,2,3]#param_array.PARAM
 
-df = pd.DataFrame(df_dict)
 
-df.to_csv(Dir+f"Results/Result_{experiment_id}/results_df.csv")
-
-# np.savetxt(Dir+f"Results/Result_{experiment_id}/y_train_truth.csv", y_train, delimiter=",")
-# np.savetxt(Dir+f"Results/Result_{experiment_id}/y_val_truth.csv", y_val, delimiter=",")
-# np.savetxt(Dir+f"Results/Result_{experiment_id}/y_test_truth.csv", y_test, delimiter=",")
-
-# np.savetxt(Dir+f"Results/Result_{experiment_id}/y_train_pred.csv", train_pred, delimiter=",")
-# np.savetxt(Dir+f"Results/Result_{experiment_id}/y_val_pred.csv", val_pred, delimiter=",")
-# np.savetxt(Dir+f"Results/Result_{experiment_id}/y_test_pred.csv", test_pred, delimiter=",")
-
+pool = multiprocessing.Pool(10, init_worker)
+pool.map(run_experiment, EXPERIMENT_IDS)
