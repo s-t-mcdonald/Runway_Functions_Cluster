5f12ba89f0ce5ce63accb95b0e2400b4718072d1
s-t-mcdonald <mcdonst@mit.edu>
2023-01-20 17:24:36 -0500

    Updated Parameters

--- parameters/param_gen.jl
+++ parameters/param_gen.jl
@@ -20,9 +20,9 @@ LOOKAHEAD = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 DATA            = ["PD", "SM", "PD_SM"]
 CONFIG_SUPPORT  = [0, 1]
 
-EPOCHS          = [300]
+EPOCHS          = [1000]
 NUMBER_TRIALS   = [100]
-PATIENCE        = [10]
+PATIENCE        = [20]
 
 param_df    = DataFrame(PARAM = Int64[], AIRPORTS = String[], LOOKAHEAD = Int64[], DATA = String[], CONFIG_SUPPORT = Int64[],
                         EPOCHS = Int64[], NUMBER_TRIALS = Int64[], PATIENCE = Int64[])
